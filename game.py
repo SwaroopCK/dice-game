6a13ad77365407c1376f5743c0191ae57e821a3a
SwaroopCK <swaroopkorde@gmail.com>
2020-11-29 12:19:21 +0530

    First Game

--- game.py
+++ game.py
@@ -6,11 +6,18 @@ def main():
     rounds = 1
 
     while rounds!=4:
-        print("Round " +str(rounds))
+        print("\nRound " +str(rounds))
         player1 = dice_roll()
         player2 = dice_roll()
         print("Player 1 Roll =" +str(player1))
         print("Player 2 Roll =" +str(player2))
+
+        if player1 == player2:
+            print("Draw")
+        elif player1>player2:
+            print("player 1 Wins!")
+        else:
+            print("player 2 wins!") 
         rounds = rounds + 1 
 
 def dice_roll():
